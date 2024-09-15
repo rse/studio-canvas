3c835402a2c17577d0785a0ec65d5bbee08f452e
Ralf S. Engelschall <rse@engelschall.com>
2024-09-15 19:25:41 +0200

    reorder presets content and more streams to front in tabs

--- src/client/app-control.vue
+++ src/client/app-control.vue
@@ -29,11 +29,70 @@
                 <tab id="presets" name="Presets">
                     <div class="desc">
                         The <b>Presets</b> can be used to load and save the entire &mdash; or just the partial &mdash;
-                        current canvas scence control state from or to persistent preset slots.
+                        current scence control state from or to persistent preset slots.
                         When loading a partial state from a preset slot, the partial state is merged
-                        onto the current state.
+                        onto the current state. First select either "all" or "none" plus a particular state group.
+                        Second, select the preset slot. Third, select the action to perform.
                     </div>
                     <div class="presets">
+                        <div class="actions3">
+                            <div class="button destructive" v-on:click="presetFiltersSelect(false)">NONE</div>
+                            <div class="button destructive" v-on:click="presetFiltersSelect(true)">ALL</div>
+                        </div>
+                        <div class="filter">
+                            <div class="button" v-bind:class="{ selected: preset.filters.canvas }"
+                                v-on:click="preset.filters.canvas = !preset.filters.canvas">
+                                Canvas
+                            </div>
+                            <div class="button" v-bind:class="{ selected: preset.filters.decal }"
+                                v-on:click="preset.filters.decal = !preset.filters.decal">
+                                Decal
+                            </div>
+                            <div class="button" v-bind:class="{ selected: preset.filters.monitor }"
+                                v-on:click="preset.filters.monitor = !preset.filters.monitor">
+                                Monitor
+                            </div>
+                            <div class="button" v-bind:class="{ selected: preset.filters.plate }"
+                                v-on:click="preset.filters.plate = !preset.filters.plate">
+                                Plate
+                            </div>
+                            <div class="button" v-bind:class="{ selected: preset.filters.hologram }"
+                                v-on:click="preset.filters.hologram = !preset.filters.hologram">
+                                Hologram
+                            </div>
+                            <div class="button" v-bind:class="{ selected: preset.filters.lights }"
+                                v-on:click="preset.filters.lights = !preset.filters.lights">
+                                Lights
+                            </div>
+                            <div class="button" v-bind:class="{ selected: preset.filters.avatars }"
+                                v-on:click="preset.filters.avatars = !preset.filters.avatars">
+                                Avatars
+                            </div>
+                            <div class="button" v-bind:class="{ selected: preset.filters.references }"
+                                v-on:click="preset.filters.references = !preset.filters.references">
+                                References
+                            </div>
+                            <div class="button" v-bind:class="{ selected: preset.filters.CAM1 }"
+                                v-on:click="preset.filters.CAM1 = !preset.filters.CAM1">
+                                CAM1
+                            </div>
+                            <div class="button" v-bind:class="{ selected: preset.filters.CAM2 }"
+                                v-on:click="preset.filters.CAM2 = !preset.filters.CAM2">
+                                CAM2
+                            </div>
+                            <div class="button" v-bind:class="{ selected: preset.filters.CAM3 }"
+                                v-on:click="preset.filters.CAM3 = !preset.filters.CAM3">
+                                CAM3
+                            </div>
+                            <div class="button" v-bind:class="{ selected: preset.filters.CAM4 }"
+                                v-on:click="preset.filters.CAM4 = !preset.filters.CAM4">
+                                CAM4
+                            </div>
+                            <div class="button" v-bind:class="{ selected: preset.filters.renderer }"
+                                v-on:click="preset.filters.renderer = !preset.filters.renderer">
+                                Renderer
+                            </div>
+                        </div>
                         <div class="slots">
                             <div class="button" v-bind:class="{ selected: preset.slot === 0 }"
                                 v-on:click="preset.slot = preset.slot !== 0 ? 0 : -1">
@@ -115,63 +174,73 @@
                                 SAVE
                             </div>
                         </div>
-                        <div class="filter">
-                            <div class="button" v-bind:class="{ selected: preset.filters.canvas }"
-                                v-on:click="preset.filters.canvas = !preset.filters.canvas">
-                                Canvas
-                            </div>
-                            <div class="button" v-bind:class="{ selected: preset.filters.decal }"
-                                v-on:click="preset.filters.decal = !preset.filters.decal">
-                                Decal
-                            </div>
-                            <div class="button" v-bind:class="{ selected: preset.filters.monitor }"
-                                v-on:click="preset.filters.monitor = !preset.filters.monitor">
-                                Monitor
-                            </div>
-                            <div class="button" v-bind:class="{ selected: preset.filters.plate }"
-                                v-on:click="preset.filters.plate = !preset.filters.plate">
-                                Plate
-                            </div>
-                            <div class="button" v-bind:class="{ selected: preset.filters.hologram }"
-                                v-on:click="preset.filters.hologram = !preset.filters.hologram">
-                                Hologram
-                            </div>
-                            <div class="button" v-bind:class="{ selected: preset.filters.lights }"
-                                v-on:click="preset.filters.lights = !preset.filters.lights">
-                                Lights
-                            </div>
-                            <div class="button" v-bind:class="{ selected: preset.filters.avatars }"
-                                v-on:click="preset.filters.avatars = !preset.filters.avatars">
-                                Avatars
-                            </div>
-                            <div class="button" v-bind:class="{ selected: preset.filters.references }"
-                                v-on:click="preset.filters.references = !preset.filters.references">
-                                References
-                            </div>
-                            <div class="button" v-bind:class="{ selected: preset.filters.CAM1 }"
-                                v-on:click="preset.filters.CAM1 = !preset.filters.CAM1">
-                                CAM1
-                            </div>
-                            <div class="button" v-bind:class="{ selected: preset.filters.CAM2 }"
-                                v-on:click="preset.filters.CAM2 = !preset.filters.CAM2">
-                                CAM2
-                            </div>
-                            <div class="button" v-bind:class="{ selected: preset.filters.CAM3 }"
-                                v-on:click="preset.filters.CAM3 = !preset.filters.CAM3">
-                                CAM3
-                            </div>
-                            <div class="button" v-bind:class="{ selected: preset.filters.CAM4 }"
-                                v-on:click="preset.filters.CAM4 = !preset.filters.CAM4">
-                                CAM4
-                            </div>
-                            <div class="button" v-bind:class="{ selected: preset.filters.renderer }"
-                                v-on:click="preset.filters.renderer = !preset.filters.renderer">
-                                Renderer
-                            </div>
+                    </div>
+                </tab>
+
+                <!--  ==== STREAMS ====  -->
+                <tab id="streams" name="Streams">
+                    <div class="desc">
+                        The <b>Streams</b> are the video streams which can be shown on
+                        the Monitor, Decal, Plate and Hologram. The content is received
+                        with a single virtual video device which had to carry N content stacks
+                        (from left to right) and each stack consists of the content RGB
+                        on top of content alpha with (black-to-white, 100-0% visibillity).
+                    </div>
+                    <div class="control">
+                        <div class="label1">video</div>
+                        <div class="label2">(device)</div>
+                        <div class="label3">[id]:</div>
+                        <div class="value">
+                            <div class="fixed">*</div>
                         </div>
-                        <div class="actions3">
-                            <div class="button destructive" v-on:click="presetFiltersSelect(false)">NONE</div>
-                            <div class="button destructive" v-on:click="presetFiltersSelect(true)">ALL</div>
+                        <div class="button" v-on:click="state.streams.device = ''">RESET</div>
+                        <div class="slider">
+                            <input class="text" v-model.lazy="state.streams.device"/>
+                        </div>
+
+                        <div class="label1">size</div>
+                        <div class="label2">(width)</div>
+                        <div class="label3">[px]:</div>
+                        <div class="value">
+                            <input tabindex="8" v-bind:value="fieldExport(state.streams.width, 0, true)"
+                                v-on:change="(ev) => state.streams.width = fieldImport((ev.target! as HTMLInputElement).value, 0, 3840)"/>
+                        </div>
+                        <div class="button" v-on:click="state.streams.width = 1920">RESET</div>
+                        <div class="slider">
+                            <slider class="slider" v-model="state.streams.width"
+                                v-bind:min="0.0" v-bind:max="3840" v-bind:step="40"
+                                show-tooltip="drag" v-bind:format="formatSliderValue" v-bind:lazy="false"
+                            ></slider>
+                        </div>
+
+                        <div class="label1">size</div>
+                        <div class="label2">(height)</div>
+                        <div class="label3">[px]:</div>
+                        <div class="value">
+                            <input tabindex="8" v-bind:value="fieldExport(state.streams.height, 0, true)"
+                                v-on:change="(ev) => state.streams.height = fieldImport((ev.target! as HTMLInputElement).value, 0, 2160)"/>
+                        </div>
+                        <div class="button" v-on:click="state.streams.height = 1080">RESET</div>
+                        <div class="slider">
+                            <slider class="slider" v-model="state.streams.height"
+                                v-bind:min="0.0" v-bind:max="2160" v-bind:step="40"
+                                show-tooltip="drag" v-bind:format="formatSliderValue" v-bind:lazy="false"
+                            ></slider>
+                        </div>
+
+                        <div class="label1">fps</div>
+                        <div class="label2">(fps)</div>
+                        <div class="label3">[fps]:</div>
+                        <div class="value">
+                            <input tabindex="8" v-bind:value="fieldExport(state.streams.fps, 2, true)"
+                                v-on:change="(ev) => state.streams.fps = fieldImport((ev.target! as HTMLInputElement).value, 0, 60)"/>
+                        </div>
+                        <div class="button" v-on:click="state.streams.fps = 30.0">RESET</div>
+                        <div class="slider">
+                            <slider class="slider" v-model="state.streams.fps"
+                                v-bind:min="0.0" v-bind:max="60.0" v-bind:step="0.01"
+                                show-tooltip="drag" v-bind:format="formatSliderValue" v-bind:lazy="false"
+                            ></slider>
                         </div>
                     </div>
                 </tab>
@@ -251,74 +320,6 @@
                             v-bind:class="{ unselectable: state.canvas.texture2 === '' }"
                             v-on:click="syncCanvas()">
                             SYNC
-                        </div>
-                    </div>
-                </tab>
-
-                <!--  ==== STREAMS ====  -->
-                <tab id="streams" name="Streams">
-                    <div class="desc">
-                        The <b>Streams</b> are the video streams which can be shown on
-                        the Monitor, Decal, Plate and Hologram. The content is received
-                        with a single virtual video device which had to carry N content stacks
-                        (from left to right) and each stack consists of the content RGB
-                        on top of content alpha with (black-to-white, 100-0% visibillity).
-                    </div>
-                    <div class="control">
-                        <div class="label1">video</div>
-                        <div class="label2">(device)</div>
-                        <div class="label3">[id]:</div>
-                        <div class="value">
-                            <div class="fixed">*</div>
-                        </div>
-                        <div class="button" v-on:click="state.streams.device = ''">RESET</div>
-                        <div class="slider">
-                            <input class="text" v-model.lazy="state.streams.device"/>
-                        </div>
-
-                        <div class="label1">size</div>
-                        <div class="label2">(width)</div>
-                        <div class="label3">[px]:</div>
-                        <div class="value">
-                            <input tabindex="8" v-bind:value="fieldExport(state.streams.width, 0, true)"
-                                v-on:change="(ev) => state.streams.width = fieldImport((ev.target! as HTMLInputElement).value, 0, 3840)"/>
-                        </div>
-                        <div class="button" v-on:click="state.streams.width = 1920">RESET</div>
-                        <div class="slider">
-                            <slider class="slider" v-model="state.streams.width"
-                                v-bind:min="0.0" v-bind:max="3840" v-bind:step="40"
-                                show-tooltip="drag" v-bind:format="formatSliderValue" v-bind:lazy="false"
-                            ></slider>
-                        </div>
-
-                        <div class="label1">size</div>
-                        <div class="label2">(height)</div>
-                        <div class="label3">[px]:</div>
-                        <div class="value">
-                            <input tabindex="8" v-bind:value="fieldExport(state.streams.height, 0, true)"
-                                v-on:change="(ev) => state.streams.height = fieldImport((ev.target! as HTMLInputElement).value, 0, 2160)"/>
-                        </div>
-                        <div class="button" v-on:click="state.streams.height = 1080">RESET</div>
-                        <div class="slider">
-                            <slider class="slider" v-model="state.streams.height"
-                                v-bind:min="0.0" v-bind:max="2160" v-bind:step="40"
-                                show-tooltip="drag" v-bind:format="formatSliderValue" v-bind:lazy="false"
-                            ></slider>
-                        </div>
-
-                        <div class="label1">fps</div>
-                        <div class="label2">(fps)</div>
-                        <div class="label3">[fps]:</div>
-                        <div class="value">
-                            <input tabindex="8" v-bind:value="fieldExport(state.streams.fps, 2, true)"
-                                v-on:change="(ev) => state.streams.fps = fieldImport((ev.target! as HTMLInputElement).value, 0, 60)"/>
-                        </div>
-                        <div class="button" v-on:click="state.streams.fps = 30.0">RESET</div>
-                        <div class="slider">
-                            <slider class="slider" v-model="state.streams.fps"
-                                v-bind:min="0.0" v-bind:max="60.0" v-bind:step="0.01"
-                                show-tooltip="drag" v-bind:format="formatSliderValue" v-bind:lazy="false"
-                            ></slider>
                         </div>
                     </div>
                 </tab>
@@ -1725,10 +1726,10 @@
             justify-items: center
             align-items: center
             gap: 10px 10px
-        .actions1
+        .actions1,
+        .actions3
             margin-right: 10px
         .actions2
-            margin-right: 20px
             display: grid
             grid-template-columns: 100px
             grid-template-rows: 290px
@@ -1736,7 +1737,7 @@
             align-items: center
             gap: 10px 10px
         .filter
-            margin-right: 10px
+            margin-right: 20px
             display: grid
             grid-template-columns: 100px 100px
             grid-template-rows: 30px 30px 30px 30px 30px 30px 30px
