215933c79e8cd6565f42912d39d6483cdc8d8468
Ralf S. Engelschall <rse@engelschall.com>
2024-09-22 17:46:28 +0200

    allow one to change the transition

--- res/media/etc/convert-i2v.bash
+++ res/media/etc/convert-i2v.bash
@@ -3,9 +3,11 @@
 ##  convert-i2v.bash -- convert two images to a video
 ##
 
-if [[ $# -ne 5 ]]; then
-    echo "USAGE:  bash convert-i2v.bash <show-sec> <fade-sec> <image1>.png <image2>.png <video>.webm" 2>&1
-    echo "SAMPLE: bash convert-i2v.bash 10 1 foo1.png foo2.png foo.webm" 2>&1
+LANG=en_US.UTF-8
+
+if [[ $# -lt 5 ]]; then
+    echo "USAGE:  bash convert-i2v.bash <show-sec> <fade-sec> <image1>.png <image2>.png <video>.webm [<transition>]" 2>&1
+    echo "SAMPLE: bash convert-i2v.bash 10 1 foo1.png foo2.png foo.webm slideleft" 2>&1
     exit 1
 fi
 
@@ -14,6 +16,7 @@ fade="$2"
 image1="$3"
 image2="$4"
 video="$5"
+transition="${6-fade}" # fade|slideleft|dissolve|pixelize
 
 total=$((2 * $show + 2 * $fade))
 fps="30"
@@ -29,8 +32,8 @@ ffmpeg \
     -framerate "$fps" -loop 1 -t "$total" -i "$image2" \
     -t "$total" \
     -filter_complex \
-        "[0:v][1:v]xfade=transition=fade:offset=$(($show / 2)):duration=$fade,format=yuva420p[v1]; \
-        [v1][0:v]xfade=transition=fade:offset=$(($show / 2 + $fade + $show)):duration=$fade,format=yuva420p[v]" \
+        "[0:v][1:v]xfade=transition=$transition:offset=$(($show / 2)):duration=$fade,format=yuva420p[v1]; \
+        [v1][0:v]xfade=transition=$transition:offset=$(($show / 2 + $fade + $show)):duration=$fade,format=yuva420p[v]" \
     -map "[v]" \
     -c:v libvpx \
     -an \
