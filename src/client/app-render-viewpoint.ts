8b3d89986acb72c271b2ea92ea15531a99bd7fc3
Ralf S. Engelschall <rse@engelschall.com>
2025-02-19 00:17:43 +0100

    add missing file

---
+++ src/client/app-render-viewpoint.ts
@@ -0,0 +1,106 @@
+/*
+**  Studio-Canvas - Real-Time Virtual Studio Canvas Rendering
+**  Copyright (c) 2023-2024 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Licensed under GPL 3.0 <https://spdx.org/licenses/GPL-3.0-only>
+*/
+
+/*  import external dependencies  */
+import * as BABYLON                from "@babylonjs/core"
+
+/*  import internal dependencies (client-side)  */
+import Config, { type CameraName } from "./app-render-config"
+import { type API }                from "./app-render-api"
+import Camera                      from "./app-render-camera"
+
+/*  import internal dependencies (shared)  */
+import { FreeDState }              from "../common/app-freed"
+import { StateTypePartial }        from "../common/app-state"
+
+/*  exported renderer feature  */
+export default class Viewpoint {
+    /*  internal state  */
+    private cameras = new Map<CameraName, Camera>()
+    private camera: CameraName
+
+    /*  object construction  */
+    constructor (
+        private api:        API,
+        private cameraName: string,
+        private ptzFreeD:   boolean,
+        private ptzKeys:    boolean,
+        private log:        (level: string, msg: string) => void
+    ) {
+        /*  instantiate individual cameras  */
+        this.cameras.set("CAM1", new Camera(api, "CAM1", log))
+        this.cameras.set("CAM2", new Camera(api, "CAM2", log))
+        this.cameras.set("CAM3", new Camera(api, "CAM3", log))
+        this.cameras.set("CAM4", new Camera(api, "CAM4", log))
+
+        /*  mapping of camera to type  */
+        const camera = cameraName as CameraName
+        const cameraType = Config.camNameToTypeMap.get(camera)
+        if (!cameraType)
+            throw new Error("invalid camera")
+        this.camera = camera
+    }
+
+    /*  establish feature  */
+    async establish () {
+        for (const cam of this.cameras.values())
+            await cam.establish()
+
+        /*  activate requested (default) camera  */
+        this.activateCamera(this.camera)
+
+        /*  allow keyboard to manually adjust camera  */
+        if (this.ptzKeys) {
+            this.api.scene.getScene().onKeyboardObservable.add((kbInfo) => {
+                if (kbInfo.type !== BABYLON.KeyboardEventTypes.KEYDOWN)
+                    return
+                this.reactOnKeyEvent(kbInfo.event.key)
+            })
+        }
+    }
+
+    /*  reflect the current scene state  */
+    async reflectSceneState (state: StateTypePartial) {
+        for (const cam of this.cameras.values())
+            await cam.reflectSceneState(state)
+    }
+
+    /*  react on a received FreeD state record by reflecting its camera PTZ state  */
+    reflectFreeDState (state: FreeDState) {
+        if (!this.ptzFreeD)
+            return
+        this.cameras.get(this.camera)?.reflectFreeDState(state)
+    }
+
+    /*  react on a key (down) event by manipulating the camera PTZ state  */
+    async reactOnKeyEvent (key: string) {
+        if (!this.ptzKeys)
+            return
+        if      (key === "1") this.activateCamera("CAM1")
+        else if (key === "2") this.activateCamera("CAM2")
+        else if (key === "3") this.activateCamera("CAM3")
+        else if (key === "4") this.activateCamera("CAM4")
+        return this.cameras.get(this.camera)?.reactOnKeyEvent(key)
+    }
+
+    /*  activate a camera  */
+    activateCamera (cameraName: CameraName) {
+        const scene  = this.api.scene.getScene()
+        const camera = this.cameras.get(cameraName)?.getCamera() ?? null
+        if (camera === null)
+            throw new Error("no such camera device")
+        const oldCamera = this.camera
+        this.camera = cameraName
+        scene.activeCamera = camera
+        return oldCamera
+    }
+
+    /*  provide current camera  */
+    currentCamera () {
+        return this.camera
+    }
+}
+
