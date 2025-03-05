c6ac3ce5659aacb47c5ab70e5bc19972a5c5e15b
Ralf S. Engelschall <rse@engelschall.com>
2025-03-05 23:40:49 +0100

    update year in all copyright messages

--- src/client/app-render-canvas-transition.fs
+++ src/client/app-render-canvas-transition.fs
@@ -1,7 +1,7 @@
 #version 300 es
 /*
 **  Studio-Canvas - Real-Time Virtual Studio Canvas Rendering
-**  Copyright (c) 2023-2024 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**  Copyright (c) 2023-2025 Dr. Ralf S. Engelschall <rse@engelschall.com>
 **  Licensed under GPL 3.0 <https://spdx.org/licenses/GPL-3.0-only>
 */
 
