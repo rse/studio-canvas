86ae7ff0c62d7512c5763aea42d5408f3ec22aa4
Ralf S. Engelschall <rse@engelschall.com>
2025-03-05 23:38:37 +0100

    fix style

--- src/client/app-render-canvas-transition.fs
+++ src/client/app-render-canvas-transition.fs
@@ -39,11 +39,11 @@ vec4 textureSampleNew (vec2 uv) {
 
 /*  utility function: random value generation  */
 float random (vec2 co) {
-    highp float a = 12.9898;
-    highp float b = 78.233;
-    highp float c = 43758.5453;
-    highp float dt= dot(co.xy, vec2(a,b));
-    highp float sn= mod(dt,3.14);
+    highp float a  = 12.9898;
+    highp float b  = 78.233;
+    highp float c  = 43758.5453;
+    highp float dt = dot(co.xy, vec2(a,b));
+    highp float sn = mod(dt,3.14);
     return fract(sin(sn) * c);
 }
 
